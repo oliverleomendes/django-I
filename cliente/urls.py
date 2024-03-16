64705edfb5d06a6c6c5525c7810f9e552d1fb554
Leonardo Mendes <email@email.com>
2024-03-16 11:32:35 -0300

    Add in clientes and estoque process update and delete

--- cliente/urls.py
+++ cliente/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index_cliente'),
     path('cadastro', views.cadastro, name='cadastro_cliente'),
-    path('criar/cliente', views.criar, name='criar_cliente')
+    path('criar/cliente', views.criar, name='criar_cliente'),
+    path('deletar/<int:id_cliente>', views.deletar_cliente, name='deletar_cliente'),
+    path('editar/<int:id_cliente>', views.editar, name='editar_cliente'),
+    path('atualizar', views.atualizar_cliente, name='atualizar_cliente')
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Clientes
 from datetime import datetime
 
@@ -16,5 +16,22 @@ def criar(request):
     cliente = Clientes.objects.create(primeiro_nome=nome, sobrenome=sobrenome,
                                         email=email, status=1)
     cliente.save()
-    return redirect('index')
-    
+    return redirect('index_cliente')
+
+def editar(request, id_cliente):
+    cliente = get_object_or_404(Clientes, pk=id_cliente)
+    return render(request, 'editar_cliente.html', {'dados_cliente': cliente})
+
+def atualizar_cliente(request):
+    id_cliente = request.POST["id"]
+    cliente = Clientes.objects.get(pk=id_cliente)
+    cliente.primeiro_nome = request.POST["nome"]
+    cliente.sobrenome = request.POST["sobrenome"]
+    cliente.email = request.POST["email"]
+    cliente.save()
+    return redirect('index_cliente')
+
+def deletar_cliente(request, id_cliente):
+    cliente = get_object_or_404(Clientes, pk=id_cliente)
+    cliente.delete()
+    return redirect('index_cliente')
