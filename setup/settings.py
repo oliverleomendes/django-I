b4c824abde942e7c46423e60d38cbbb44a799ace
Leonardo Mendes <email@email.com>
2024-03-23 08:51:49 -0300

    Add in project app Usuario

--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cliente',
     'estoque',
+    'usuario',
     'bootstrap5'
 ]
 
--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('usuario.urls')),
     path('clientes/', include('cliente.urls')),
     path('estoque/', include('estoque.urls'))
 ]
