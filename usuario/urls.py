17a6c3504d77d5f72d3238712375a21df947bd3f
Leonardo <leonardo.moliveira@senac.sp.br>
2024-04-12 20:06:26 -0300

    Add process auth

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index_usuario')
+    path('', views.index, name='index_usuario'),
+    path('criar', views.criar, name='criar_usuario'),
+    path('login', views.login, name='login_usuario'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
+    return render(request, 'index_usuario.html')
+
+def cadastro(request):
+    return render(request, 'cadastro_usuario.html')
+
+def criar(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('index_usuario')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('index_usuario')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('index_usuario')
+        
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
+
+        return redirect('index_usuario')
+    else:
+        return render(request, 'index_usuario.html')
+    
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if email == "" or senha == "":
+            print('Os campos email e senha não podem ficar em branco')
+            return redirect('index_usuario')
+        
+        print(email, senha)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('index_estoque')
+    
     return render(request, 'index_usuario.html')
